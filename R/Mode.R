85ea8cad129cd68ec21d77de2a5f5ce0aa7ce299
Brian Stephen Ssali <brianstefans@gmail.com>
2022-06-30 14:51:16 +0300

    fixed files from the conflicts

--- R/Mode.R
+++ R/Mode.R
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-#' Select the most common value
-#'
-#' @param x A list of vectors.
-#'
-#' @return A vector.
-#' @export
-#'
-#' @examples
-#' list_of_items<- c(1,2,2,3)
-#' Mode(list_of_items)
-#'
-
-Mode <- function(x) {
-  ux <- unique(x)
-  ux <- ux[is.na(ux)==FALSE]
-  ux[which.max(tabulate(match(x, ux)))]
-}
-=======
 #' Select the most common value and return it
 #'
 #' @param x A list of vectors.
@@ -34,4 +15,3 @@ Mode <- function(x) {
   ux <- ux[is.na(ux)==FALSE]
   ux[which.max(tabulate(match(x, ux)))]
 }
->>>>>>> 47fda30b5afbdf487eb5f3c346aab1b6a368e4a9
--- R/hello.R
+++ R/hello.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Hello, world!
 #
 # This is an example function named 'hello'
@@ -18,24 +17,3 @@
 hello <- function() {
   print("Hello, world!")
 }
-=======
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-#' @export
-
-hello <- function() {
-  print("Hello, world!")
-}
->>>>>>> 47fda30b5afbdf487eb5f3c346aab1b6a368e4a9
